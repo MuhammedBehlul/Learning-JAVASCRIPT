a56d5c6808db627ca2b03089403858e716f10397
Muhammed Behlul <mubsimsek@gmail.com>
2023-08-17 14:32:30 +0300

    login page button- contact page editting

---
+++ Exercises/Pie-Shop/script.js
@@ -0,0 +1,8 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    const loginButton = document.querySelector(".login-button");
+
+    loginButton.addEventListener("click", function () {
+        window.location.href = "loginpage.html";
+    });
+});
