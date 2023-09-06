a35aff56e453e115db47c99e3cedac2634479522
Muhammed Behlul <mubsimsek@gmail.com>
2023-09-06 15:28:30 +0300

    making animation

--- JavaScript/Drum Kit Challenge/Drum Kit Starting Files/index.js
+++ JavaScript/Drum Kit Challenge/Drum Kit Starting Files/index.js
@@ -11,12 +11,14 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
 
         soundPlay(this.innerHTML);
+        buttonAnimation(this.innerHTML);
 
     });
 
     document.addEventListener("keydown", function (event) {
 
         keyPressSoundPlay(event.key);
+        buttonAnimation(event.key);
 
     });
 }
@@ -108,4 +110,17 @@ function keyPressSoundPlay(key) {
             break;
 
     }
+}
+
+
+function buttonAnimation(currentKey) {
+
+    var activeButton = document.querySelector("." + currentKey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function () {
+        activeButton.classList.remove("pressed");
+    }, 100);
+
 }
