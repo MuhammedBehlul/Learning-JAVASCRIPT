aa5462dd82e23909b50b3a9baaef3107e8496b8d
Muhammed Behlul <mubsimsek@gmail.com>
2023-09-12 10:34:56 +0300

    dropdown menu added

---
+++ Hakmar-Website/Hakmar Website/Codes/hakmar.js
@@ -0,0 +1,8 @@
+
+document.querySelector("#button-online-siparis").addEventListener("click", function () {
+    window.location.href = "online-siparis.html";
+})
+
+document.querySelector("#button-haberdar-olun").addEventListener("click", function () {
+    window.location.href = "haberdar-olun.html";
+})
