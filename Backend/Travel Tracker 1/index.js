5d5cb93b626ba8f01219c30d661de3f16e02d70c
Muhammed Behlul <mubsimsek@gmail.com>
2024-08-07 18:09:57 +0300

    index js

--- Backend/Travel Tracker 1/index.js
+++ Backend/Travel Tracker 1/index.js
@@ -10,14 +10,14 @@ const db = new pg.Client({
   host: "localhost",
   database: "World",
   password: "muhoreyiz",
-  port: 5432
+  port: 5432,
 });
-
 db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// GET home page
 app.get("/", async (req, res) => {
   const result = await db.query("SELECT country_code FROM visited_countries");
   let countries = [];
@@ -26,7 +26,25 @@ app.get("/", async (req, res) => {
   });
   console.log(result.rows);
   res.render("index.ejs", { countries: countries, total: countries.length });
-  db.end();
+});
+
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+
+  const result = await db.query(
+    "SELECT country_code FROM countries WHERE country_name = $1",
+    [input]
+  );
+
+  if (result.rows.length !== 0) {
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
+      countryCode,
+    ]);
+    res.redirect("/");
+  }
 });
 
 app.listen(port, () => {
