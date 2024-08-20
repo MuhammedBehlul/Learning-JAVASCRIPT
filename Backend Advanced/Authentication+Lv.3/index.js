0cc3417faafd6e1df0d72e068992c25a8495e7a0
Muhammed Behlul <mubsimsek@gmail.com>
2024-08-20 17:19:53 +0300

    cookies

--- Backend Advanced/Authentication+Lv.3/index.js
+++ Backend Advanced/Authentication+Lv.3/index.js
@@ -19,7 +19,11 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: { maxAge: 24 * 60 * 60 * 1000 },
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000, // 1 day
+      secure: false, // Set to true if using HTTPS
+      httpOnly: true,
+    },
   })
 );
 
