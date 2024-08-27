ec6fae894b97812cf322f27227859f12a8ddffea
Muhammed Behlul <mubsimsek@gmail.com>
2024-08-27 11:25:34 +0300

    react starting

--- Backend Advanced/Authentication+Lv.3/index.js
+++ Backend Advanced/Authentication+Lv.3/index.js
@@ -21,7 +21,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       maxAge: 24 * 60 * 60 * 1000, // 1 day
-      secure: false, // Set to true if using HTTPS
+      secure: false,
       httpOnly: true,
     },
   })
